20cec05303c078f5c6b85db5cba98bf8929da5c5
RidhoAji921 <ridhoaji921@gmail.com>
2025-03-04 17:02:56 +0700

    nambah chart buat beda paket sama pet hotel

--- app/Livewire/BusinessReport.php
+++ app/Livewire/BusinessReport.php
@@ -2,14 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\PetHotelReservation;
 use App\Models\Reservation;
 use Carbon\Carbon;
 use Livewire\Component;
 
 class BusinessReport extends Component
 {
-    public $ordersData = [];
-    public $finishedData = [];
+    public $ordersData_MBS = [];
+    public $ordersData_MLK = [];
+    public $ordersData_PetHotel = [];
+    public $finishedData_MBS = [];
+    public $finishedData_MLK = [];
     public $dates = [];
 
     function mount() {
@@ -22,18 +26,31 @@ class BusinessReport extends Component
 
         $period = Carbon::parse($firstOrderDate)->toPeriod($lastOrderDate, '1 day');
         $this->dates = collect($period)->map(fn($date) => $date->format('Y-m-d'))->toArray();
-        $orders = Reservation::selectRaw('DATE(created_at) as date, COUNT(id) as total')
+        $orders = Reservation::selectRaw('DATE(created_at) as date, COUNT(id) as total
+            , SUM(CASE WHEN package_id = 1 THEN 1 ELSE 0 END) as package_1_total
+            , SUM(CASE WHEN package_id = 2 THEN 1 ELSE 0 END) as package_2_total')
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get();
+            // ->pluck('package_1_total', 'date');
+        $finishedOrders = Reservation::selectRaw('DATE(finished_at) as date
+            , SUM(CASE WHEN package_id = 1 THEN 1 ELSE 0 END) as package_1_total
+            , SUM(CASE WHEN package_id = 2 THEN 1 ELSE 0 END) as package_2_total')
+            ->whereNotNull('finished_at')
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get();
+        $ordersHotel = PetHotelReservation::selectRaw('DATE(created_at) as date, COUNT(id) as total')
             ->groupBy('date')
             ->orderBy('date')
             ->pluck('total', 'date');
-        $finishedOrders = Reservation::selectRaw('DATE(finished_at) as date, COUNT(id) as total')
-            ->whereNotNull('finished_at') // Hanya hitung order yang memiliki finished_at
-            ->groupBy('date')
-            ->orderBy('date')
-            ->pluck('total', 'date');
+        
         foreach ($this->dates as $date) {
-            $this->ordersData[] = $orders[$date] ?? 0;
-            $this->finishedData[] = $finishedOrders[$date] ?? 0;
+            $this->ordersData_MBS[] = $orders->pluck('package_1_total', 'date')[$date] ?? 0;
+            $this->finishedData_MBS[] = $finishedOrders->pluck('package_1_total', 'date')[$date] ?? 0;
+            $this->ordersData_MLK[] = $orders->pluck('package_2_total', 'date')[$date] ?? 0;
+            $this->finishedData_MLK[] = $finishedOrders->pluck('package_2_total', 'date')[$date] ?? 0;
+            $this->ordersData_PetHotel[] = $ordersHotel[$date] ?? 0;
         }
     }
 
